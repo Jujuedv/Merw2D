e3d2768982757b2d5d0263e8093fdbed0825ec8e
Merwco <marco-neumann96@gmx.de>
2015-08-22 00:56:56 +0200

    main.cpp cleanup

--- Contentmgr.cpp
+++ Contentmgr.cpp
@@ -2,30 +2,33 @@
 
 Contentmgr::Contentmgr(SDL_Renderer* renderer)
 {
-  textureload(renderer);
+	textureload(renderer);
 }
 
 Contentmgr::~Contentmgr()
 {
-  unloadAll();
+	unloadAll();
 }
 
 void Contentmgr::unloadAll()
 {
-  for(int e = 0; e < loadedTextures.size(); e++)
-  {
-    SDL_DestroyTexture(loadedTextures[e]);
-  }
-  loadedTextures.clear();
+	for(int e = 0; e < loadedTextures.size(); e++)
+	{
+		SDL_DestroyTexture(loadedTextures[e]);
+	}
+	loadedTextures.clear();
 }
 
 void Contentmgr::textureload(SDL_Renderer* renderer)
 {
-  SDL_Texture* texture = IMG_LoadTexture(renderer, "cityTiles_089.png");
-  loadedTextures.push_back(texture);
+	//SDL_Surface* tempsurf = IMG_LoadPNG_RW("test.png");
+	SDL_Texture* texture = IMG_LoadTexture(renderer, "test.png");
+	if(texture == NULL)
+		std::cout << "Failed to load image" << std::endl;
+	loadedTextures.push_back(texture);
 }
 
 SDL_Texture* Contentmgr::getTexture(int index)
 {
-  return loadedTextures[index];
+	return loadedTextures[index];
 }
--- main.cpp
+++ main.cpp
@@ -5,69 +5,14 @@
 int resX = 1024;
 int resY = 768;
 
-std::mt19937 random_gen;
-
-int map[50][50];
-int visible[16][12];
-int camposX = 0;
-int camposY = 0;
-
-void renderScreen(SDL_Renderer* renderer)
-{
-	for(int y = 0; y < 12; y++)
-	{
-		for(int x = 0; x < 16; x++)
-		{
-			SDL_Rect rect;
-			rect.h = 64;
-			rect.w = 64;
-			rect.x = x * 64;
-			rect.y = y * 64;
-			SDL_SetRenderDrawColor(renderer, 255, visible[x][y], 0, SDL_ALPHA_OPAQUE);
-			SDL_RenderFillRect(renderer, &rect);
-		}
-	}
-}
-
-void getVisibleScreen(int ScreenposX, int ScreenposY)
-{
-	for(int y = 0; y < 12; y++)
-	{
-		for(int x = 0; x < 16; x++)
-		{
-			visible[x][y] = map[ScreenposX + x][ScreenposY + y];
-		}
-	}
-}
-
-void scrolling()
-{
-	const Uint8 *keystate = SDL_GetKeyboardState(nullptr);
-	if(keystate[SDL_SCANCODE_RIGHT] && camposX < 50 - 16)
-		camposX++;
-	if(keystate[SDL_SCANCODE_LEFT] && camposX > 0)
-		camposX--;
-	if(keystate[SDL_SCANCODE_UP] && camposY > 0)
-		camposY--;
-	if(keystate[SDL_SCANCODE_DOWN] && camposY < 50 - 12)
-		camposY++;
-}
-
 int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
+	IMG_Init(IMG_INIT_PNG);
 	SDL_Window* window = SDL_CreateWindow("2DEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, resX, resY, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
 
 	bool running = true;
-	std::clock_t oldtime = std::clock();
-	for(int x = 0; x < 50; x++)
-	{
-		for(int y = 0; y < 50; y++)
-		{
-			map[x][y] = std::uniform_int_distribution<int>(1, 255)(random_gen);
-		}
-	}
 
 	Isorenderer iso;
 	Contentmgr cont(renderer);
@@ -88,12 +33,6 @@ int main(int argc, char* argv[])
 				}
 				break;
 			}
-		}
-		std::clock_t time = std::clock();
-		if((double)time - (double)oldtime >= 100)
-		{
-			scrolling();
-			oldtime = time;
 		}
 
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
